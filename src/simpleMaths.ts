d6c35b07ed94389648f8544329a0a64fd1950b07
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-18 09:42:27 +0000

    circle area

--- src/simpleMaths.ts
+++ src/simpleMaths.ts
@@ -1,3 +1,6 @@
+const promptSync = require('prompt-sync');
+const prompt = promptSync();
+
 export function calcAreaSquare(sideLength: number): number {
     return Math.pow(sideLength, 2);
 }
@@ -6,3 +9,12 @@ export function calcAreaTriangle(width: number, height: number): number {
     return width * height / 2;
 }
 
+function circle(){
+    const pi:number = 3.14159265358979323
+    
+    const choice:string = prompt("Do you want to calculate circle area or circumference? a/c\n");
+    if(choice = "a"){
+        const radius:string = prompt("What is the radius of the circle?");
+        (radius * radius) * pi
+    }
+}
